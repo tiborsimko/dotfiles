460d9c01a731e50f627ccff26320bb80887d202f
Tibor Šimko <tibor.simko@cern.ch>
2020-03-15 22:31:47 +0100

    barpyrus: load average, disk free, net up/down
    Signed-off-by: Tibor Šimko <tibor.simko@cern.ch>

--- barpyrus/.config/barpyrus/config.py
+++ barpyrus/.config/barpyrus/config.py
@@ -26,10 +26,12 @@ bat_icons = [
 # first icon: 0 percent
 # last icon: 100 percent
 bat_delta = 100 / len(bat_icons)
-conky_text = '%{F\\#9fbc00}%{T2}\ue026%{T-}%{F\\#989898}${cpu}% '
+conky_text = '%{F\\#9fbc00}%{T2}\ue026%{T-}%{F\\#989898}${loadavg} '
+conky_text += '%{F\\#9fbc00}%{T2}\ue026%{T-}%{F\\#989898}${cpu}% '
 conky_text += '%{F\\#9fbc00}%{T2}\ue021%{T-}%{F\\#989898}${memperc}% '
-#conky_text += '%{F\\#9fbc00}%{T2}\ue13c%{T-}%{F\\#989898}${downspeedf}K '
-#conky_text += '%{F\\#9fbc00}%{T2}\ue13b%{T-}%{F\\#989898}${upspeedf}K '
+conky_text += '%{F\\#9fbc00}%{T2}\ue021%{T-}%{F\\#989898}${fs_free} '
+conky_text += '%{F\\#9fbc00}%{T2}\ue13c%{T-}%{F\\#989898}${downspeedf wlp4s0}K '
+conky_text += '%{F\\#9fbc00}%{T2}\ue13b%{T-}%{F\\#989898}${upspeedf wlp4s0}K '
 conky_text += "${if_existing /sys/class/power_supply/BAT0}"
 conky_text += "%{T2}"
 conky_text += "${if_match \"$battery\" == \"discharging $battery_percent%\"}"
