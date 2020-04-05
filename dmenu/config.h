10c8c24e444c7b0fbf994a12eba220303b85233c
Tibor Šimko <tibor.simko@cern.ch>
2020-04-05 13:42:54 +0200

    dmenu: new "menu" or "status bar" colour scheme
    Uses new "menu" or "status bar" colour scheme for both WM and
    applications: (i) background normal: gruvbox bg0 #282828, (ii)
    foreground normal: gruvbox grey #928374, (iii) foreground selected:
    gruvbox fg1 #ebdbb2.
    
    Signed-off-by: Tibor Šimko <tibor.simko@cern.ch>

--- dmenu/config.h
+++ dmenu/config.h
@@ -11,7 +11,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#928374", "#282828" },
-	[SchemeSel] = { "#b8bb26", "#282828" },
+	[SchemeSel] = { "#ebdbb2", "#282828" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
