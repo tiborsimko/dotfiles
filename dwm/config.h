25d48fb585b90bfef3fcd19b36ddaf36fa2b94b3
Tibor Šimko <tibor.simko@cern.ch>
2020-04-05 14:16:37 +0200

    dwm: larger scratchpad terminal
    Signed-off-by: Tibor Šimko <tibor.simko@cern.ch>

--- dwm/config.h
+++ dwm/config.h
@@ -67,7 +67,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL  };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "144x42", NULL  };
 
 static void focusmaster(const Arg *arg) {
     focus(nexttiled(selmon->clients));
