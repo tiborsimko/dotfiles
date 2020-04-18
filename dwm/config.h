3a6a6b2da31b59e7e81e77cdc7436ebed3c73cfd
Tibor Šimko <tibor.simko@cern.ch>
2020-04-19 00:51:16 +0200

    dwm: removal of scratchpad patch
    Removes scratchpad patch. It happened a few times that I launched an app
    there that I wanted to bring along as a regular full-size tillable
    window while working on another tag. It seems easier to use a coherent
    "all-windows-tagged, no-window-hidden" mental concept for UX consistency
    and simplicity.
    
    After removal of scratchpad, changes shortcuts Mod4-c for clipboard menu
    and Mod4-s for switch view.
    
    Signed-off-by: Tibor Šimko <tibor.simko@cern.ch>

--- dwm/config.h
+++ dwm/config.h
@@ -67,8 +67,6 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "144x42", NULL  };
 static const char *clipmenucmd[] = { "clipmenu", "-i", NULL  };
 
 static void focusmaster(const Arg *arg) {
@@ -80,8 +78,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_s,      togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = clipmenucmd } },
+	{ MODKEY,                       XK_c,      spawn,          {.v = clipmenucmd } },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_n,      focusstack,     {.i = +1 } },
@@ -99,7 +96,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_p,      pushup,         {0} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_c,      view,           {0} },
+	{ MODKEY,                       XK_s,      view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
