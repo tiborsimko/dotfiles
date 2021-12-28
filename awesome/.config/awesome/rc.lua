277ced4e1bcd6cacaa51b5d2f32434d45be94db1
Tibor Šimko <tibor.simko@cern.ch>
2021-12-29 00:27:38 +0100

    awesome: fix xterm tiled window size
    AwesomeWM respects window size hits by default, so that e.g. xterm
    windows were created with perfect size from the number-of-characters
    point of view (depending on the available space but also on the font
    size). This was however making tiled windows "unequally sized" from the
    number-of-pixels point of view when several xterm clients appeared on
    the same tag or when an xterm client appeared next to browser on the
    same tag.
    
    This commit asks AwesomeWM to disregard client size hits, making all
    tiled windows nicely equal in size. The "extra" space where xterm cannot
    print whole characters at the given font size anymore is simply treated
    as a sort of "internal padding".

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -513,6 +513,7 @@ awful.rules.rules = {
                      keys = clientkeys,
                      buttons = clientbuttons,
                      screen = awful.screen.preferred,
+                     size_hints_honor = false,
                      placement = awful.placement.no_overlap+awful.placement.no_offscreen
      }
     },
